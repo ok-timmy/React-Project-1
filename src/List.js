198345e926d1b0b03f6e809f908cd19e8562a2c9
ok-timmy <94664656+ok-timmy@users.noreply.github.com>
2022-02-10 04:10:07 +0100

    Add files via upload

---
+++ src/List.js
@@ -0,0 +1,53 @@
+const people = [
+    {
+        "name": "Oladimeji Afeez",
+        "age": "24 years",
+        "gender": "Male",
+        "id" : 1,
+        "img": "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+    },
+    {
+        "name": "Aina Ekundayo",
+        "age": "20 years",
+        "gender": "Male",
+        "id" : 2,
+        "img" : "https://images.pexels.com/photos/1516680/pexels-photo-1516680.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+    },
+    {
+        "name": "Ajewole Bunmi",
+        "age": "20 years",
+        "gender": "Female",
+        "id" : 3,
+        "img":"https://images.pexels.com/photos/3992656/pexels-photo-3992656.png?auto=compress&cs=tinysrgb&dpr=1&w=500"
+    },
+    {
+        "name": "Oduntan Toluwani",
+        "age": "27 years",
+        "gender": "Male",
+        "id" : 4,
+        "img": "https://images.pexels.com/photos/2182970/pexels-photo-2182970.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+    },
+    {
+        "name": "Komolafe Olaitan",
+        "age": "21 years",
+        "gender": "Female",
+        "id" : 5,
+        "img" : " https://images.pexels.com/photos/3916455/pexels-photo-3916455.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+    },
+    {
+        "name": "Okere Joy",
+        "age": "24 years",
+        "gender": "Female",
+        "id" : 6,
+        "img" : "https://images.pexels.com/photos/3746210/pexels-photo-3746210.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+    },
+    {
+        "name": "Okunola Timilehin",
+        "age": "23 years",
+        "gender": "Male",
+        "id" : 7,
+        "img": "https://images.pexels.com/photos/1181391/pexels-photo-1181391.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+    }
+]
+
+export default people;
